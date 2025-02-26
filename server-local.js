7512f7cd556adbad9fa59b0226d2fd6ec6827af8
elmerrodriguez <rb.elmer@hotmail.com>
2025-02-25 22:02:13 -0500

    cambios envio

--- server-local.js
+++ server-local.js
@@ -128,6 +128,90 @@ app.post("/send-emails", async (req, res) => {
     res.end();
 });
 
+app.post("/onli-send-emails", async (req, res) => {
+    const datos = req.body;
+    res.setHeader("Content-Type", "application/json");
+
+    // Crear un agente HTTPS que ignore certificados autofirmados
+    const httpsAgent = new https.Agent({
+        rejectUnauthorized: false,
+    });
+
+    let channelListen = datos.channelListen;
+    //console.log('channelListen:', channelListen);
+    for (const venta of datos.ventas) {
+		let pedido = {
+			documenttypeId: datos.documenttypeId,
+			venta: {
+                nota_sale_id: venta.nota_sale_id,
+                id: venta.id,
+                details: venta.details,
+            },
+			serie: datos.serie,
+			enline: datos.enline,
+			local: datos.local,
+			userId: datos.userId
+		}
+
+        try {
+            // 1. Enviar solicitud para generar la boleta
+            const response = await axios.post(datos.apiBackenStepOne, { 'pedido': pedido }, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-CSRF-TOKEN": datos.csrfToken
+                },
+                httpsAgent, // Pasar el agente personalizado
+            });
+            console.log(response.data)
+            // Extraer datos de la respuesta
+            const { success, document,onlisaleId, message } = response.data;
+        
+            // 2. Emitir resultado al usuario en tiempo real
+            io.emit(channelListen, {
+                status: success,
+                step: 1,
+                data: { document, message }
+            });
+        
+            // Si la boleta se generó correctamente, proceder con el envío del correo
+            if (success) {
+                const emailData = {
+                    person_email: document.client_email,
+                    person_name: document.client_rzn_social,
+                    document_id: document.id,
+                    onlisaleId: onlisaleId
+                };
+        
+                // 3. Enviar solicitud para el envío del correo
+                const emailResponse = await axios.post(datos.apiBackenStepTwo, emailData, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        "X-CSRF-TOKEN": datos.csrfToken
+                    },
+                    httpsAgent,
+                });
+                console.log(emailResponse.data)
+                // 4. Emitir confirmación de envío de correo
+                io.emit(channelListen, {
+                    status: emailResponse.data.success,
+                    step: 2,
+                    data: emailResponse.data.correosMessage
+                });
+            }
+        } catch (error) {
+            console.error("Error en el proceso:", error.message);
+            
+            // Emitir error al usuario
+            io.emit(channelListen, {
+                status: "error",
+                error: error.message
+            });
+        }
+    }
+
+    res.end();
+});
+
 // Iniciar servidor HTTPS
 server.listen(3000, () => {
     console.log('HTTPS server running on port 3000');
