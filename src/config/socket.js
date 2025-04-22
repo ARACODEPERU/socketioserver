f9422ab56721f73bd887fa8bce3b5d75f304ac3a
elmerrodriguez <rb.elmer@hotmail.com>
2025-04-22 11:59:06 -0500

    camp

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,24 +1,49 @@
+// const socketIO = require('socket.io');
+
+// function setupSocket(server) {
+//     const io = socketIO(server, {
+//         cors: {
+//             origin: "*", // En producción, especifica el dominio permitido
+//             methods: ["GET", "POST","PUT"],
+//             allowedHeaders: ["Access-Control-Allow-Origin", "Content-Type"],
+//             credentials: true
+//         }
+//     });
+
+//     io.on('connection', (socket) => {
+//         socket.on('disconnect', () => {
+//             console.log('Cliente desconectado ARACODE');
+//         });
+//     });
+
+//     global.io = io; // Hacerlo accesible en otras partes del código
+
+//     console.log('socket.io: ACTIVO')
+// }
+
+// module.exports = { setupSocket };
+
 const socketIO = require('socket.io');
 
 function setupSocket(server) {
     const io = socketIO(server, {
         cors: {
-            origin: "*", // En producción, especifica el dominio permitido
-            methods: ["GET", "POST","PUT"],
-            allowedHeaders: ["Access-Control-Allow-Origin", "Content-Type"],
-            credentials: true
+            origin: "*", // Esto permite cualquier origen. Reemplaza con tu dominio en producción
+            methods: ["GET", "POST","PUT","DELETE"],
+            allowedHeaders: ["Content-Type"],
+            credentials: false
         }
     });
 
     io.on('connection', (socket) => {
-        console.log('Nuevo cliente conectado ARACODE');
+        console.log('Cliente conectado');
+
         socket.on('disconnect', () => {
             console.log('Cliente desconectado ARACODE');
         });
     });
 
-    global.io = io; // Hacerlo accesible en otras partes del código
-
+    global.io = io;
     console.log('socket.io: ACTIVO')
 }
 
