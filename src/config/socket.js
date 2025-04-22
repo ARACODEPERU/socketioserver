dd944e1804995a0541d43417dc6f6f02652bb8f3
elmerrodriguez <rb.elmer@hotmail.com>
2025-04-22 13:43:42 -0500

    Update socket.js

--- src/config/socket.js
+++ src/config/socket.js
@@ -1,50 +1,26 @@
-// const socketIO = require('socket.io');
-
-// function setupSocket(server) {
-//     const io = socketIO(server, {
-//         cors: {
-//             origin: "*", // En producción, especifica el dominio permitido
-//             methods: ["GET", "POST","PUT"],
-//             allowedHeaders: ["Access-Control-Allow-Origin", "Content-Type"],
-//             credentials: true
-//         }
-//     });
-
-//     io.on('connection', (socket) => {
-//         socket.on('disconnect', () => {
-//             console.log('Cliente desconectado ARACODE');
-//         });
-//     });
-
-//     global.io = io; // Hacerlo accesible en otras partes del código
-
-//     console.log('socket.io: ACTIVO')
-// }
-
-// module.exports = { setupSocket };
-
 const socketIO = require('socket.io');
 
 function setupSocket(server) {
     const io = socketIO(server, {
         cors: {
-            origin: "*", // Esto permite cualquier origen. Reemplaza con tu dominio en producción
-            methods: ["GET", "POST","PUT","DELETE"],
-            allowedHeaders: ["Content-Type"],
-            credentials: false
+            origin: "*",  // Asegúrate de que esté configurado para aceptar cualquier origen, o especifica tu dominio
+            methods: ["GET", "POST", "PUT"],
+            allowedHeaders: ["Content-Type", "Authorization"],
+            credentials: true,
         }
     });
 
     io.on('connection', (socket) => {
         console.log('Cliente conectado');
-
+        
         socket.on('disconnect', () => {
-            console.log('Cliente desconectado ARACODE');
+            console.log('Cliente desconectado');
         });
     });
 
-    global.io = io;
-    console.log('socket.io: ACTIVO')
+    global.io = io; // Hacerlo accesible en otras partes del código
+
+    console.log('Socket.io funcionando');
 }
 
 module.exports = { setupSocket };
